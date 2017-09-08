1da4198a42c9d6080a6d1f228e3bfb7b2f6b672b
VirMJY <1159081156@qq.com>
2017-09-08 10:18:44 +0800

    Map and String

---
+++ VirMJY_Lib/vy_Map.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef VY_MAP_H_
+#define VY_MAP_H_
+namespace vstl
+{
+    template<typename Type>
+    class Map
+    {
+    public:
+        Map();
+    protected:
+    private:
+    }
+}
+#endif
---
+++ VirMJY_Lib/vy_String.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef VY_STRING_H_
+#define VY_STRING_H_
+
+namespace vstl
+{
+    
+}
+
+#endif
